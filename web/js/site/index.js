89632cf888af9e514f96629fd407afbf99a46909
Leandro Nascimento <nleeandro@gmail.com>
2020-01-14 23:59:53 -0300

    implements total sales view for user

--- web/js/site/index.js
+++ web/js/site/index.js
@@ -1,6 +1,7 @@
 (function($){
 
 	var $keys;
+	var form = $("#form-venda");
 	$('.btn-success').on('click', function(){
 		$keys = $("#grid").yiiGridView('getSelectedRows');
 		console.log($keys);
@@ -9,12 +10,29 @@
 			type: 'POST',
 			data: {ids: $keys},
 			success:function(data){
-				console.log(data);
+				if(data.message == 'error')
+					form[0].reset();
+				else
+					location.reload();
 			}
 		});
 	});
 
-	//console.log(document.location);
-
+	var rows = $("#form-venda tbody tr");
+	var label_price = $(".badge-price");
+	var total_price = parseFloat($(".badge-price").text().replace(/[^0-9,.]/g, ''));
+	rows.each(function($e){
+		let siblings = $(this).find("td").siblings();
+		$(this).find("input[type='checkbox']").on('click', function(){
+			let price = siblings.last().html();
+			price = price.replace(/[^0-9.,]/g, '').replace(',', '.');
+			if($(this).is(':checked'))
+				total_price = total_price+parseFloat(price);
+			else
+				total_price = total_price-parseFloat(price);
+			price_string = 'R$ '+total_price.toFixed(2).toString().replace('.', ',');
+			$('.badge-price').text(price_string);
+		});
+	});
 
 })(jQuery);
